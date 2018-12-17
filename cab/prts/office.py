7e7cdd56b7d948e12f393025d2cb6c8d5b86c42f
jay <75509151@qq.com>
2018-12-17 14:50:59 +0800

    fix print_file in office

--- cab/prts/office.py
+++ cab/prts/office.py
@@ -3,7 +3,7 @@ import os
 def print_file(document, prniter_name):
     if not os.path.isfile(document):
         raise Exception("not file")
-    cmd = "/usr/bin/libreoffice --headless --invisible --pt '%s' 'document'" % (printer_name, document)
+    cmd = "/usr/bin/libreoffice --headless --invisible --pt '%s' '%s'" % (printer_name, document)
     return os.system(cmd)
 
 
