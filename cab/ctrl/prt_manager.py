c98e99abbb5722f9f6ee53f7cc8b5764292bbcda
jay <75509151@qq.com>
2018-12-23 11:56:00 +0800

    update prt_manager

--- cab/ctrl/prt_manager.py
+++ cab/ctrl/prt_manager.py
@@ -3,3 +3,28 @@
 class PrtManager(object):
     def __init__(self):
         pass
+
+    def get_printer(self):
+        pass
+
+    def check_printer(self):
+        self._check_and_install()
+        self._check_and_report_params()
+        self.check_and_report_status()
+
+
+    def _check_and_install(self):
+        pass
+
+    def _check_and_report_params(self):
+        pass
+
+    def check_and_report_status(self):
+        pass
+
+    def print_file(self, document):
+        pass
+    
+
+    def report_print_result(self):
+        pass
